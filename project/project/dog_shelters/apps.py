08f13fed712767e9b09ea28ed5ed039c40c6348c
gabriel melero montero <gabrix177@hotmail.com>
2023-03-31 13:48:40 +0200

    Refactor DogShelter app class

--- project/project/dog_shelters/apps.py
+++ project/project/dog_shelters/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class dog_sheltersConfig(AppConfig):
+class DogSheltersConfig(AppConfig):
+    default_auto_field='django.db.models.BigAutoField'
     name = 'dog_shelters'
